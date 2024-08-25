41b46862bbb35ef1a59d401e710c7b2c60576245
LULU CHEN <xiaoluer.chen@gmail.com>
2024-08-25 22:30:08 +1200

    fixed front-end

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 if (process.env.NODE_ENV === 'development') {
